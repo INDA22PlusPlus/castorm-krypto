e441e4068cb2577411dae879d11c3580abf3e18c
castormann <castormannen@gmail.com>
2022-12-08 22:03:35 +0100

    crypto

---
+++ src/sign.py
@@ -0,0 +1,77 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+def encrypt_and_sign(data, private_key):
+
+    public_key = private_key.public_key()
+
+    encrypted_data = public_key.encrypt(
+        data,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    signature = private_key.sign(
+        encrypted_data,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    return encrypted_data, signature
+
+
+
+def decrypt_and_verify(encrypted_data, signature, private_key):
+
+    private_key.public_key().verify(
+        signature,
+        encrypted_data,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    data = private_key.decrypt(
+        encrypted_data,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    return data
+
+def key_gen():
+
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+
+    return private_key
+
+
+def test_encrypt_decrypt():
+    private_key = key_gen()
+    data = b"my secret data"
+
+    encrypted_data, signature = encrypt_and_sign(data, private_key)
+
+    decrypted_data = decrypt_and_verify(encrypted_data, signature, private_key)
+
+    print(data)
+    print(decrypted_data)
+
+if __name__ == "__main__":
+    test_encrypt_decrypt()
+
